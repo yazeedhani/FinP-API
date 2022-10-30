feafa940bc6c5ec1b625611e0ddbfcd30c3fa613
yazeedhani <haniyazeed9@gmail.com>
2022-10-30 10:43:12 -0600

    feat: add 404 error handling

--- app/routes/account_routes.js
+++ app/routes/account_routes.js
@@ -20,11 +20,12 @@ router.get('/account/:userId', requireToken, async (req, res, next) => {
     try {
         const loggedInUserId = req.params.userId
         const userAccount = await Account.findOne({owner: loggedInUserId}).populate('recurrences').populate('monthTrackers')
+        await handle404(userAccount)
         requireOwnership(req, userAccount)
         res.status(200).json({ account: userAccount.toObject() })   
     }
-    catch(error) {
-        console.log('Error:', error)
+    catch(err) {
+        next(err)
     }
 })
 
@@ -35,8 +36,8 @@ router.patch('/account/:userId', requireToken, removeBlanks, async (req, res, ne
         await Account.findOneAndUpdate({owner: loggedInUserId}, {income: req.body.account.income, loans: req.body.account.loans})
         res.sendStatus(204)
     }
-    catch(error) {
-        console.log('Error:', error)
+    catch(err) {
+        next(err)
     }
 })
 
@@ -47,6 +48,7 @@ router.delete('/account/:userId/:recurringId', requireToken, async (req, res, ne
         const recurringId = req.params.recurringId
 
         const userAccount = await Account.findOne({owner: loggedInUserId})
+        await handle404(userAccount)
         requireOwnership(req, userAccount)
         let expenseIndex
         // const expenseIndex = account.recurrences.indexOf(recurringId)
@@ -60,8 +62,8 @@ router.delete('/account/:userId/:recurringId', requireToken, async (req, res, ne
 
         await userAccount.save()
     }
-    catch(error) {
-        console.log('Error:', error)
+    catch(err) {
+        next(err)
     }
 })
 
